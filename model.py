089f53f39a3cb24855ee6c8f42f4bd8a06cb03b8
Daniyar <daniyar.turmukhambetov.10@ucl.ac.uk>
2016-09-02 20:52:42 +0100

    mnist dataset

---
+++ model.py
@@ -0,0 +1,19 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from datetime import datetime
+import os.path
+import time
+import math
+
+import numpy as np
+from six.moves import xrange  # pylint: disable=redefined-builtin
+import tensorflow as tf
+
+import flags
+
+class GAN(object):
+  
+  def __init__(self):
+    pass
